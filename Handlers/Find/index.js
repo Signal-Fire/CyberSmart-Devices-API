72a23e771c7c371373bc60949dc43b08496c54c1
Henry <henrypye@outlook.com>
2018-03-18 18:49:43 +0000

    Implement map into FindConnected()

--- Handlers/Find/index.js
+++ Handlers/Find/index.js
@@ -46,16 +46,12 @@ class Find {
             var child = exec('arp -ani wlan0', function(err, stdout, stderr) {
                 if (stderr) 
                     return reject("Unable to find any devices");
-                
-                var deviceInfo = {};
-                var deviceList = [];
 
-                stdout.split("\n").filter(x => !x.includes("incomplete")).forEach(line => {
+                var deviceList = stdout.split("\n").filter(x => !x.includes("incomplete")).map(line => {
                     var splitLine = line.split(" ");
                     deviceInfo.ip = splitLine[1];
                     deviceInfo.mac = splitLine[3];
-                    deviceList.push(deviceInfo);
-                    deviceInfo = {};
+                    return deviceInfo;
                 });
 
                 return resolve(deviceList);  

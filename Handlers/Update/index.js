d4a957863e16c172d39d2335fa3c629692ad49fe
Henry <henrypye@outlook.com>
2018-04-20 09:42:04 +0100

    Updater now updates state!

--- Handlers/Update/index.js
+++ Handlers/Update/index.js
@@ -1,9 +1,38 @@
 var Device = require('../../Models/Device'),
-    config = require('../../Configuration');
+    config = require('../../Configuration'),
+    axios = require('axios');
 
 class Update {
     constructor() {
 
+    }
+
+    UpdateState(device) {
+        return new Promise(function(resolve, reject) {
+            Device.findByIdAndUpdate(device.deviceId, { state : device.state}, function(err, result) {                
+                if (err || result === null)
+                    return reject(err);
+
+                return resolve(result);
+            })        
+        });
+        /*return new Promise(function(resolve, reject) {
+            axios({
+                method: 'POST',
+                url : config["state-url"] + '/changestate',
+                data : {
+                    address : address,
+                    state : deviceState
+                }
+            }).then(res => {
+                if (res.status !== 200)
+                    return reject("State not updated");
+
+                     
+            }).catch(err => {
+                return reject(err);
+            });
+        });*/
     }
 }
 

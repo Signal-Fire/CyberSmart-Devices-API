c3828edb8c5434cc148257f1d40309491fd6d9b3
Henry <henrypye@outlook.com>
2018-04-25 17:58:49 +0100

    Altered UpdateState method

--- Handlers/Update/index.js
+++ Handlers/Update/index.js
@@ -9,29 +9,28 @@ class Update {
 
     UpdateState(device) {
         return new Promise(function(resolve, reject) {
-            Device.findByIdAndUpdate(device.deviceId, { state : device.state}, function(err, result) {                
-                if (err || result === null)
-                    return reject(err);
+            axios({
+                method: 'POST',
+                url : config["state-url"] + '/update/state',
+                data : {
+                    address : result.address,
+                    state : device.state
+                }
+            }).then(res => {
+                if (res.status !== 200)
+                    return reject("State not updated, issue posting");
+                
+                Device.findByIdAndUpdate(device.id, { state : device.state }, function(err, result) {
+                    if (err || result === null)
+                        return reject(err);
 
-                axios({
-                    method: 'POST',
-                    url : config["state-url"] + '/update/state',
-                    data : {
-                        address : result.address,
-                        state : device.state
-                    }
-                }).then(res => {
-                    if (res.status !== 200)
-                        return reject("State not updated, issue posting");
-    
-                    return resolve("State updated");    
-                }).catch(err => {
-                    return reject(err);
-                }); 
+                    return resolve(result);
+                });
 
-                return resolve(result);
-            }) 
-                 
+                return resolve("State updated");    
+            }).catch(err => {
+                return reject(err);
+            });         
         });
     }
 }
